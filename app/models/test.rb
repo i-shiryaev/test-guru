461fa1c35b84dc183fda79d3a4d811df5156e03d
Ivan S <i.shiryaev@protonmail.com>
2018-10-02 19:22:56 +0700

    Rewrite methods for User and Test models with scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,8 +8,7 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-
-  def self.by_category(name)
-    joins(:category).where(categories: { title: name }).order(title: :desc).pluck(:title)
-  end
+  scope :by_category, ->(name) {
+    joins(:category).where(categories: { title: name }).order(title: :desc).pluck(:title) }
+  scope :by_level, ->(level) { where(level: level) }
 end
